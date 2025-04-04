ca136f7c8e2d0b983749deede41a32b34a65b27a
yen <yen@LAPTOP-JR0ECM7S>
2025-04-04 21:02:03 +0800

    Update 優化寫入後 data

--- 平行處理/Program.cs
+++ 平行處理/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,18 +16,37 @@ namespace 平行處理
             Stopwatch stopwatch = new Stopwatch();
             CSVHelper helper = new CSVHelper();
 
+            int ROW_COUNT = 5000;
+            double readTime = 0;
+            double writeTime = 0;
+
+            Console.WriteLine($"筆數: {ROW_COUNT}");
+
+            string inputPath = $"C:\\Users\\user\\source\\repos\\C#基礎專案\\平行處理\\Input\\MOCK_DATA_{ROW_COUNT}.csv";
+            string outputPath = $"C:\\Users\\user\\source\\repos\\C#基礎專案\\平行處理\\Output\\MOCK_DATA_{ROW_COUNT}.csv";
+
+            if (Directory.Exists($"C:\\Users\\user\\source\\repos\\C#基礎專案\\平行處理\\Output"))
+            {
+                Directory.Delete($"C:\\Users\\user\\source\\repos\\C#基礎專案\\平行處理\\Output", true);
+                Directory.CreateDirectory($"C:\\Users\\user\\source\\repos\\C#基礎專案\\平行處理\\Output");
+            }
+
             stopwatch.Start();
-
-            List<CSVdata> list = helper.Read<CSVdata>("C:\\Users\\user\\source\\repos\\C#基礎專案\\平行處理\\Input\\MOCK_DATA_1000000.csv");
-
-            string outputPath = "C:\\Users\\user\\source\\repos\\C#基礎專案\\平行處理\\Output\\MOCK_DATA_1000000.csv";
-
-            helper.WriteList(outputPath, list);
-
+            List<CSVdata> list = helper.Read<CSVdata>(inputPath);
             stopwatch.Stop();
+            readTime = stopwatch.ElapsedMilliseconds / 1000.0;
+            Console.WriteLine("讀取: " + readTime + "s");
 
 
-            Console.WriteLine(stopwatch.ElapsedMilliseconds / 1000.0 + "s");
+            stopwatch.Restart();
+            helper.WriteList(outputPath, list);
+            stopwatch.Stop();
+            writeTime = stopwatch.ElapsedMilliseconds / 1000.0;
+
+            Console.WriteLine("寫入: " + stopwatch.ElapsedMilliseconds / 1000.0 + "s");
+
+
+            Console.WriteLine("任務完成，總耗時:" + (readTime + writeTime) + "s");
             Console.ReadLine();
 
         }
